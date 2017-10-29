eee964ab62bc972779dffcb4c6700a8b3bda4a2b
Naveen MV <naveenmv7@gmail.com>
2017-10-29 11:32:48 +0530

    added tables create

--- texttosql.py
+++ texttosql.py
@@ -1,4 +1,4 @@
-college_info = [['AERONAUTICAL ENGINEERING', 
+college_info = [['AERONAUTICAL ENGINEERING',
 [['ACE16AN007', [['BE103', 'C']]],
 ['ACE16AN012', [['EC100', 'Nochange'], ['BE100', 'Nochange']]],
 ['ACE16AN014', [['BE100', 'Nochange']]], ['ACE16AN016', [['BE100', 'Nochange']]],
@@ -39,3 +39,43 @@ college_info = [['AERONAUTICAL ENGINEERING',
 ['ACE16ME036', [['BE100', 'Nochange']]]]]]
 
 def make_sql(l):
+
+def create_tables():
+    with open("./database.sql","w+") as f:
+        table = """
+CREATE TABLE college (
+    college_id  serial PRIMARY KEY,
+    college_name   varchar(80) NOT NULL
+);
+
+CREATE TABLE department (
+     dept_id   serial PRIMARY KEY,
+     dept_name   varchar(50) NOT NULl,
+     colleg_id  serial references college(colleg_id)
+);
+
+CREATE TABLE course (
+    course_id serial PRIMARY KEY,
+    course_code varchar(20) UNIQUE
+);
+
+CREATE TABLE dept_student (
+    register_id varchar(15) PRIMARY KEY,
+    dept_id serial references department(dept_id)
+);
+
+CREATE TABLE student_marks (
+    mark_id serial PRIMARY KEY,
+    register_id varchar(15) references dept_student(register_id)
+    course_id serial references course(course_id)
+);
+
+CREATE TABLE dept_course (
+    dc_id serial PRIMARY KEY,
+    dept_id serial references department(dept_id),
+    course_id serial references course(course_id)
+);
+"""
+
+if __name__ == '__main__':
+    create_tables()
